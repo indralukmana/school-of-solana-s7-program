e62976ec40e3ea98b83ec4b89af88618c26e861d
Indra Lukmana <indra@indralukmana.com>
2026-01-14 14:14:48 +0700

    refactor: replace plan account fields with content hash and content uri

--- anchor_project/programs/plan-vault/src/state/plan.rs
+++ anchor_project/programs/plan-vault/src/state/plan.rs
@@ -4,15 +4,7 @@ use anchor_lang::prelude::*;
 #[derive(InitSpace)]
 pub struct Plan {
     pub vault_account: Pubkey,
-    #[max_len(100)]
-    pub plan_title: String,
-    #[max_len(100)]
-    pub trading_platform: String,
-    #[max_len(100)]
-    pub risk_level: String,
-    #[max_len(10)]
-    pub ticker: String,
-    pub investment_amount: u64,
-    pub stop_loss_bps: u64,
-    pub take_profit_bps: u64,
+    pub content_hash: [u8; 32],
+    #[max_len(200)]
+    pub content_uri: String,
 }
